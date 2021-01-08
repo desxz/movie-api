66ec4f0f64f4e7faf2303da579f7802b266dc8a3
desxz <muratgun@posta.mu.edu.tr>
2021-01-08 18:16:40 +0300

    travis-ci

--- helper/db.js
+++ helper/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://admin:admin123@movie-api.jp0fs.mongodb.net/movie-api?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:admin123@movie-api.jp0fs.mongodb.net/movie-api?retryWrites=true&w=majority"
 
 module.exports= () => {
     mongoose.set('useNewUrlParser', true);
